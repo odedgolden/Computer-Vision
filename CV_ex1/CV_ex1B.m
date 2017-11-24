3f0ffa44f31d37299b86168803a7772d603e03df
Oded Golden <odedgolden@gmail.com>
2017-11-24 03:08:17 +0200

    Working on the masks of Canny.

--- CV_ex1/CV_ex1B.m
+++ CV_ex1/CV_ex1B.m
@@ -12,32 +12,35 @@
 close all;
 clear all;
 image_name = 'Alicia2.jpg';
-C = canny(image_name, 1,1,1)
+C = canny(image_name, 2,1,1)
 imshow(C);
 
 %   MAIN FUNCTION:
 
 function E = canny(imagePath, sigma, L_th, H_th)
-I = imread(imagePath);
-ID = im2double(I);
-mask_size = 16;
+% I = imread(imagePath);
+% ID = im2double(I);
+mask_size = 2*sigma;
+
 % Create the Gaussian kernels:
 G_dx = Deriv_Gauss_x(sigma, mask_size)
 G_dy = Deriv_Gauss_y(sigma, mask_size)
 
-E = I;
+% E = I;
 end
 
-% HELPER FUNCTIONS
+%   HELPER FUNCTIONS
 
-function D = Deriv_Gauss_x(sigma, mask_size)
+function Dx = Deriv_Gauss_x(sigma, mask_size)
 [x,y] = meshgrid(-mask_size:mask_size, -mask_size:mask_size)
 D = exp(-(x.^2 + y.^2)/(2*sigma^2))
 D = D./sum(D(:))
+Dx = -x.*D/sigma^2
 end
 
-function D = Deriv_Gauss_y(sigma, mask_size)
+function Dy = Deriv_Gauss_y(sigma, mask_size)
 [x,y] = meshgrid(-mask_size:mask_size, -mask_size:mask_size)
 D = exp(-(x.^2 + y.^2)/(2*sigma^2))
 D = D./sum(D(:))
+Dy = -y.*D/sigma^2
 end
