ace4ebf0a83c0367f0866906db7bf3e13241b926
Oded Golden <odedgolden@gmail.com>
2017-12-23 21:17:06 +0200

    CV_ex2 - Fixed the goddamn epipolar lines.

--- CV_ex2/draw_epipolar_lines.m
+++ CV_ex2/draw_epipolar_lines.m
@@ -3,10 +3,10 @@ function [f1,f2]=draw_epipolar_lines(im1,im2,F,p,f1,f2)
 %     epipolar_line = epipolar_line./epipolar_line(3)
 %     epipolar_line = epipolar_line(1:2)
     
-    x_q = 1;
+    x_q = 1
     y_q = round(-(epipolar_line(3)+epipolar_line(1))/epipolar_line(2))
     
-    x_p = size(im1,2);
+    x_p = size(im2,2)
     y_p = round(-(epipolar_line(3)+epipolar_line(1)*x_p)/epipolar_line(2))
-    
-    plot(x_q,y_q,x_p,y_p)
+    hold on
+    plot([x_q,x_p],[y_q,y_p])
