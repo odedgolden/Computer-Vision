5cce7b9c340e1579f887cb83d4030c284cb8d83a
Oded Golden <odedgolden@gmail.com>
2018-01-05 14:30:28 +0200

    CV_ex2- Sort of maybe finished A.

--- CV_ex3/OF.m
+++ CV_ex3/OF.m
@@ -1,1 +1,6 @@
 function [U,V, M,O]=OF(F1,F2, Sigma_S, Region)
+% Smooth the figures using sigma (the special domain)
+% Smooth the image in the temporal domain..?
+% Compute A and b
+% Compute G
+% If rank(G)=2, compute u and v
--- CV_ex3/Script3.m
+++ CV_ex3/Script3.m
@@ -11,6 +11,7 @@ for i=1:size(mov,4)
     hold on;
     
     %%% put here your optical flow results on im and its successive frame using quiver
+    quiver(OF())
     pause(0.1);
     hold off;
 end
